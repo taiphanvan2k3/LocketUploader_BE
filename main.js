781618c928e614345b24fdbe7b6a132023eb9d80
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-10-07 16:40:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const errorHandler = require("./src/helpers/error-handler.js");
 const app = express();
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://locket-uploader-phi.vercel.app"],
+        origin: ["http://localhost:10000", "https://locket-uploader-phi.vercel.app"],
         methods: ["GET", "POST"],
 
         // Nhằm cho phép client gửi cookie lên server
